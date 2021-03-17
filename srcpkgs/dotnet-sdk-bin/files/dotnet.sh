3d92c63d4531850fdb4ce918be3d11dd87831d47
andry-dev <andry-dev@users.noreply.github.com>
2021-03-17 08:52:42 +0100

    dotnet-sdk-bin: remove LibreSSL hack

--- srcpkgs/dotnet-sdk-bin/files/dotnet.sh
+++ srcpkgs/dotnet-sdk-bin/files/dotnet.sh
@@ -3,4 +3,4 @@
 export DOTNET_ROOT=/opt/dotnet
 export PATH="$PATH:$DOTNET_ROOT"
 # Needed to be able to work with LibreSSL
-export CLR_OPENSSL_VERSION_OVERRIDE=48
+# export CLR_OPENSSL_VERSION_OVERRIDE=48
